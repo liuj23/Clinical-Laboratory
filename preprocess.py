3d42c18cd8398f7c4b30da2033f0426804330e7b
Omar Suleman <OSuleman@gatech.edu>
2018-04-04 17:36:36 -0600

    Process Calgary Cost data

--- preprocess.py
+++ preprocess.py
@@ -42,7 +42,7 @@ def preprocess_volumes_data():
                 total = int(np.sum(extract))
                 if points > 24:
                     title = domain + ' ' + zone
-                    fileout = 'processed\V' + str(total) + '_N' + str(points) + '_' + domain + '_' + zone
+                    fileout = 'processed\\volumes\\V' + str(total) + '_N' + str(points) + '_' + domain + '_' + zone
                     fileout = fileout.replace(' ', '_')
                     plt.plot(extract, marker='.')
                     plt.xlabel('Year')
@@ -63,109 +63,174 @@ def preprocess_volumes_data():
                     continue
 
 
-def format_cls_expenses():
-    root_dir = 'input_data/CLS_expenses/'
-    reqd_cols = ['cost_type', 'bill_code', 'date', 'cost', 'org_cd', 'site_cd', 'fn_cd']
-    filenames = ['amortization', 'supplies', 'total_excl_amort', 'salaries', 'benefits']
+def parse_bill_code(bill_code):
+    org_cd, site_cd, fn_cd = bill_code.split('.')
+    return [org_cd, site_cd, fn_cd]
+
+
+def parse_bill_code_df(df):
+    cds = df.apply(parse_bill_code)
+    cds = np.asarray(cds.tolist())
+    return cds
+
+def load_cls_expenses(root_dir='input_data/CLS_expenses/',
+                         filenames=['amortization', 'supplies', 'total_excl_amort', 'salaries', 'benefits']):
     combined = None
     for filename in filenames:
         fullname = root_dir + filename + '.csv'
         data = pd.read_csv(fullname)
         data['Fiscal Period'] = data['Fiscal Period'].fillna(method='ffill')
         data['source_file'] = filename
+        try:
+            data['Fiscal Period'] = pd.to_datetime(data['Fiscal Period'], format='%b-%y')
+        except ValueError as e:
+            data['Fiscal Period'] = pd.to_datetime(data['Fiscal Period'], format='%y-%b')
         if combined is None:
             combined = data
         else:
-            combined.append(data)
-    cls_cost_grouping = pd.read_csv('input_data/reference_tables/cls_cost_grouping.csv')
-    output = pd.merge(combined, cls_cost_grouping, on='source_file', how='left')
-    output['cost'] = output['Actual MTD']*output['multiplier']
-    cds = output['AHS Functional String'].apply(parse_bill_code)
-    cds = np.asarray(cds.tolist())
-    output['org_cd'] = -1
-    output['site_cd'] = -1
-    output['fn_cd'] = -1
-    output[['org_cd', 'site_cd', 'fn_cd']] = cds
-    output['bill_code'] = output['AHS Functional String']
-    output['date'] = output['Fiscal Period']
-    ref_dept = pd.read_csv('input_data/reference_tables/site_codes.csv')
+            combined = combined.append(data)
+    return combined
+
+
+def add_cls_expenses_conformed_columns(df):
+    # Parse Functional String
+    df['org_cd'] = -1
+    df['site_cd'] = -1
+    df['fn_cd'] = -1
+    df[['org_cd', 'site_cd', 'fn_cd']] = parse_bill_code_df(df['AHS Functional String'])
+    df['org_cd'] = df['org_cd'].astype(int)
+    df['site_cd'] = df['site_cd'].astype(int)
+    df['fn_cd'] = df['fn_cd'].astype(np.int64)
+
+    # Load Reference Tables
+    ref_cls_cost_grouping = pd.read_csv('input_data/reference_tables/ref_cls_cost_grouping.csv')
+    ref_site_muni = pd.read_csv('input_data/reference_tables/ref_site_codes.csv')
     ref_fn_cd = pd.read_csv('input_data/reference_tables/ref_fn_cd.csv')
-    output['fn_cd'] = output['fn_cd'].astype(np.int64)
-    output = pd.merge(output, ref_fn_cd, on='fn_cd', how='left')
-    dept_domain = pd.read_csv('input_data/reference_tables/dept_domain_ref.csv')
-    output = pd.merge(output, dept_domain, on='Department', how='left')
+    ref_dept_domain = pd.read_csv('input_data/reference_tables/ref_dept_domain.csv')
+    ref_muni_zone = pd.read_csv('input_data/reference_tables/ref_muni_zone.csv')
+    ref_muni_zone['Municipality'] = ref_muni_zone['Municipality'].apply(lambda x: str.title(x))
+    ref_muni_zone['Zone'] = ref_muni_zone['Zone'].apply(lambda x: str.title(x))
+
+    # Add Table Columns
+    df = pd.merge(df, ref_cls_cost_grouping, on='source_file', how='left')
+    df['cost'] = df['Actual MTD'] * df['multiplier']
+    print 'WARNING: missing cost groupings for source files', df[df['multiplier'].isnull()]['source_file'].unique()
+    df = pd.merge(df, ref_site_muni, on='site_cd', how='left')
+    print 'WARNING: missing Municipalities for site codes:', df[df['Municipality'].isnull()]['site_cd'].unique()
+    df = pd.merge(df, ref_fn_cd, on='fn_cd', how='left')
+    print 'WARNING: missing Departments for functional centers:', df[df['Department'].isnull()]['fn_cd'].unique()
+    df = pd.merge(df, ref_dept_domain, on='Department', how='left')
+    print 'WARNING: missing Domains for site codes:', df[df['Domain'].isnull()]['Department'].unique()
+    df = pd.merge(df, ref_muni_zone, on='Municipality', how='left')
+    print 'WARNING: missing Zones for Municipality:', df[df['Zone'].isnull()]['Municipality'].unique()
+
+    return df
+
+
+def file_agg_writer(df, grouped_cols, agg_col):
+    all_cols = grouped_cols + agg_col
+    df = df[all_cols]
+    grouped = df.groupby(grouped_cols)
+    output = grouped.aggregate(np.sum)
+    # print output.index.levels[:-2]
+    # print output.index.get_level_values('cost_type')
+    assumed_struct = ['cost_type', 'domain', 'zone']
+    print 'WARNING: Assuming structure', assumed_struct
+    cost_types = output.index.levels[0]
+    domains = output.index.levels[1]
+    zones = output.index.levels[2]
+    i = 1
+    print output.head()
+    for cost_type in cost_types:
+        for domain in domains:
+            for zone in zones:
+                print cost_type, domain, zone
+                try:
+                    extract = output.loc[cost_type, domain, zone]
+                    points = extract.shape[0]
+                    total = int(np.sum(extract))
+                    if points > 24:
+                        title = cost_type + ' ' + domain + ' ' + zone
+                        fileout = 'processed\\costs\\C' + str(total) + '_N' + str(points) + '_' + cost_type + ' ' + \
+                                  domain + '_' + zone
+                        fileout = fileout.replace(' ', '_')
+                        plt.plot(extract, marker='.')
+                        plt.xlabel('Year')
+                        plt.ylabel('Cost')
+                        plt.title(title)
+                        plt.savefig(fileout)
+                        plt.clf()
+                        print 'writing ' + fileout
+                        extract['date'] = extract.index
+                        extract.index = range(points)
+                        extract['index'] = range(points)
+                        extract[['index', 'date', 'cost']].to_csv(fileout + '.csv', index=False)
+                        i += 1
+                    else:
+                        print 'NOT WRITING ', domain, zone, 'because not enough samples:', points, 'Volume:', total
+                except Exception as e:
+                    # print 'exception', e, type(e)
+                    continue
+
+
+def format_cls_expenses():
+    reqd_cols = ['cost', 'cost_type', 'Domain', 'Zone', 'Fiscal Period']
+    combined = load_cls_expenses()
+    combined = add_cls_expenses_conformed_columns(combined)
+    agg_col = ['cost']
+    grouping_cols = ['cost_type', 'Domain', 'Zone', 'Fiscal Period']
+    file_agg_writer(combined, grouping_cols, agg_col)
+    return
+    grouped = output.groupby(grouping_cols)
+
+    # print tv['Domain'].unique()
+    output = grouped.aggregate(np.sum)
+    cost_types = output.index.levels[0]
+    domains = output.index.levels[1]
+    zones = output.index.levels[2]
+    i = 1
+    for cost_type in cost_types:
+        for domain in domains:
+            for zone in zones:
+                try:
+                    extract = output.loc[cost_type, domain, zone]
+                    print extract.head()
+                    print type(extract.index[0])
+                    plt.plot(extract, marker='.')
+                    plt.xlabel('Year')
+                    plt.ylabel('Cost')
+                    title = cost_type + ' ' + domain + ' ' + zone
+                    plt.title(title)
+                    plt.show()
+                    plt.clf()
+                # points = extract.shape[0]
+                # total = np.sum(extract)
+                # # print points, total, extract
+                # if points > 24:
+                #     title = domain + ' ' + zone
+                #     fileout = 'processed\\cost\\C' + str(total) + '_N' + str(points) + '_' + domain + '_' + zone
+                #     fileout = fileout.replace(' ', '_')
+                #     plt.plot(extract, marker='.')
+                #     plt.xlabel('Year')
+                #     plt.ylabel('Cost')
+                #     plt.title(title)
+                #     plt.savefig(fileout)
+                #     plt.clf()
+                #     print 'writing ' + fileout
+                #     extract['date'] = extract.index
+                #     extract.index = range(points)
+                #     extract['index'] = range(points)
+                #     extract[['index', 'date', 'Count']].to_csv(fileout + '.csv', index=False)
+                #     i += 1
+                # else:
+                #     print 'NOT WRITING ', domain, zone, 'because not enough samples:', points, 'Cost:', total
+                except Exception as e:
+                    print 'exception', e
+                    continue
+
     # print output
-
-    output[['fn_cd', 'fn_desc', 'fin_group', 'Department', 'Domain']].to_csv('temp.csv')
-    print output.columns
-
-
-    # print combined[reqd_cols]
-
-
-
-
-
-    # ammort = pd.read_csv('input_data/CLS_expenses/amortization.csv')
-    # # benefits = pd.read_csv('input_data/CLS_expenses/benefits.csv')
-    # # salaries = pd.read_csv('input_data/CLS_expenses/salaries.csv')
-    # supplies = pd.read_csv('input_data/CLS_expenses/supplies.csv')
-    # total = pd.read_csv('input_data/CLS_expenses/total_excl_amort.csv')
-
-
-
-
-    # ammort['cost_type'] = 'ammort'
-    # benefits['cost_type'] = 'benefits'
-    # salaries['cost_type'] = 'salaries'
-    # supplies['cost_type'] = 'supplies'
-    # total['cost_type'] = 'total'
-
-
-
-
-    # print ammort.columns
-    # print benefits.columns
-    # print salaries.columns
-    # print supplies.columns
-    # print total.columns
     #
-    # columns = ['Fiscal Period', 'Actual MTD', 'AHS Functional String']
-    # print ammort[columns]
-    # print benefits[columns]
-    # print salaries[columns]
-    # print supplies[columns]
-    # print total[columns]
-
-    # print ammort.columns, benefits.columns, salaries.columns, supplies.columns, total.columns
-    #
-    # print ammort.head()
-    # print benefits.head()
-    # print salaries.head()
-    # print supplies.head()
-    # print total.head()
-
-
-    # cls_costs = pd.read_csv('input_data/CLS_total_expenses.csv', thousands=',')
-    # # print cls_costs['AHS_FUNCTIONAL_STR'].unique()
-    # # print np.diff(cls_costs.transpose().iloc[3:, 1].astype(float))
-    # # cls_costs.iloc[:, :3] = \
-    # cls_costs.index = cls_costs['AHS_FUNCTIONAL_STR']
-    # output = cls_costs.iloc[:, 3:].stack()
-    # output['cost_type'] = cls_costs['ExpenseDetails'][0]
-    # print output
-    # # output = cls_costs['AHS_FUNCTIONAL_STR'].apply(parse_bill_code)
-    # # output = np.asarray(output.tolist())
-    # # cls_costs['org_cd'] = -1
-    # # cls_costs['site_cd'] = -1
-    # # cls_costs['fn_cd'] = -1
-    # # cls_costs[['org_cd', 'site_cd', 'fn_cd']] = output
-    # #
-    # # print cls_costs
-
-def parse_bill_code(bill_code):
-    org_cd, site_cd, fn_cd = bill_code.split('.')
-    return [org_cd, site_cd, fn_cd]
+    # output[['fn_cd', 'fn_desc', 'fin_group', 'Department', 'Domain']].to_csv('temp.csv')
 
 
 def format_ahs_expenses():
@@ -198,18 +263,10 @@ def format_ahs_expenses():
     return input
 
 
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     # preprocess_volumes_data()
+    # df = load_cls_expenses()
+    # add_cls_expenses_conformed_columns(df)
     format_cls_expenses()
     # parse_bill_code('221.0000.71105009975')
     # print format_ahs_expenses()
-
