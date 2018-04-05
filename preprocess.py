197e6a902d8aae9dbd294b168ca1ec5248b54d6f
Omar Suleman <OSuleman@gatech.edu>
2018-04-05 17:29:49 -0600

    refactored code.

--- preprocess.py
+++ preprocess.py
@@ -1,66 +1,28 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import date
 
 
 def preprocess_volumes_data():
-    ref_dept = pd.read_csv('input_data\dept_domain_ref.csv')
-    tv = pd.read_csv('input_data\prov_test_volumes.csv')
+    ref_dept = pd.read_csv('input_data\\reference_tables\\ref_dept_domain.csv')
+    tv = pd.read_csv('input_data\\test_volumes\\prov_test_volumes.csv')
     tv = pd.merge(tv, ref_dept, on='Department')
     tv['Month_Year'] = pd.to_datetime(tv['Month_Year'], format='%Y-%m')
-    # print tv.columns
-    # print tv['Department'].unique()
-
     included_service_type = ['Testing']
     excluded_domains = ['Procurement - Exclude', 'Only MHDL Category - Exclude']
     include_filters = {'Service_Type': included_service_type}
     exclude_filters = {'Domain': excluded_domains}
-
     for key in include_filters:
         tv = tv[tv[key].isin(include_filters[key])]
 
     for key in exclude_filters:
         tv = tv[~tv[key].isin(exclude_filters[key])]
-
     agg_col = ['Count']
-    grouping_cols = ['Domain', 'Performing Zone', 'LIS', 'Month_Year']
-    grouping_cols = ['Performing Zone', 'Domain', 'Month_Year']
-    grouped = tv.groupby(grouping_cols)
-
-    # print tv['Domain'].unique()
-    output = grouped.aggregate(np.sum)
-    print output
-    domains = output.index.levels[0]
-    zones = output.index.levels[1]
-    lises = output.index.levels[2]
-    i = 1
-    for domain in domains:
-        for zone in zones:
-            try:
-                extract = output.loc[domain, zone]
-                points = extract.shape[0]
-                total = int(np.sum(extract))
-                if points > 24:
-                    title = domain + ' ' + zone
-                    fileout = 'processed\\volumes\\V' + str(total) + '_N' + str(points) + '_' + domain + '_' + zone
-                    fileout = fileout.replace(' ', '_')
-                    plt.plot(extract, marker='.')
-                    plt.xlabel('Year')
-                    plt.ylabel('Volume')
-                    plt.title(title)
-                    plt.savefig(fileout)
-                    plt.clf()
-                    print 'writing ' + fileout
-                    extract['date'] = extract.index
-                    extract.index = range(points)
-                    extract['index'] = range(points)
-                    extract[['index', 'date', 'Count']].to_csv(fileout + '.csv', index=False)
-                    i += 1
-                else:
-                    print 'NOT WRITING ', domain, zone, lis, 'because not enough samples:', points, 'Volume:', total
-            except Exception as e:
-                    print 'exception', e
-                    continue
+    # grouping_cols = ['Domain', 'Performing Zone', 'LIS', 'Month_Year']
+    grouping_cols = ['Domain', 'Performing Zone', 'Month_Year']
+    file_agg_writer(tv, grouping_cols, agg_col, root='processed\\test\\')
+    return
 
 
 def parse_bill_code(bill_code):
@@ -72,6 +34,7 @@ def parse_bill_code_df(df):
     cds = df.apply(parse_bill_code)
     cds = np.asarray(cds.tolist())
     return cds
+
 
 def load_cls_expenses(root_dir='input_data/CLS_expenses/',
                          filenames=['amortization', 'supplies', 'total_excl_amort', 'salaries', 'benefits']):
@@ -102,6 +65,7 @@ def add_cls_expenses_conformed_columns(df):
     df['site_cd'] = df['site_cd'].astype(int)
     df['fn_cd'] = df['fn_cd'].astype(np.int64)
 
+
     # Load Reference Tables
     ref_cls_cost_grouping = pd.read_csv('input_data/reference_tables/ref_cls_cost_grouping.csv')
     ref_site_muni = pd.read_csv('input_data/reference_tables/ref_site_codes.csv')
@@ -123,54 +87,42 @@ def add_cls_expenses_conformed_columns(df):
     print 'WARNING: missing Domains for site codes:', df[df['Domain'].isnull()]['Department'].unique()
     df = pd.merge(df, ref_muni_zone, on='Municipality', how='left')
     print 'WARNING: missing Zones for Municipality:', df[df['Zone'].isnull()]['Municipality'].unique()
-
     return df
 
 
-def file_agg_writer(df, grouped_cols, agg_col):
+def get_title(tupleName):
+    if type(tupleName) == str:
+        return tupleName.replace(' ', '_')
+    name = ''
+    print tupleName
+    for i in tupleName:
+        # print i
+        name += '_' + str(i)
+    return name.replace(' ', '_')
+
+
+def file_agg_writer(df, grouped_cols, agg_col, root='processed\\test\\'):
     all_cols = grouped_cols + agg_col
     df = df[all_cols]
     grouped = df.groupby(grouped_cols)
     output = grouped.aggregate(np.sum)
-    # print output.index.levels[:-2]
-    # print output.index.get_level_values('cost_type')
-    assumed_struct = ['cost_type', 'domain', 'zone']
-    print 'WARNING: Assuming structure', assumed_struct
-    cost_types = output.index.levels[0]
-    domains = output.index.levels[1]
-    zones = output.index.levels[2]
-    i = 1
-    print output.head()
-    for cost_type in cost_types:
-        for domain in domains:
-            for zone in zones:
-                print cost_type, domain, zone
-                try:
-                    extract = output.loc[cost_type, domain, zone]
-                    points = extract.shape[0]
-                    total = int(np.sum(extract))
-                    if points > 24:
-                        title = cost_type + ' ' + domain + ' ' + zone
-                        fileout = 'processed\\costs\\C' + str(total) + '_N' + str(points) + '_' + cost_type + ' ' + \
-                                  domain + '_' + zone
-                        fileout = fileout.replace(' ', '_')
-                        plt.plot(extract, marker='.')
-                        plt.xlabel('Year')
-                        plt.ylabel('Cost')
-                        plt.title(title)
-                        plt.savefig(fileout)
-                        plt.clf()
-                        print 'writing ' + fileout
-                        extract['date'] = extract.index
-                        extract.index = range(points)
-                        extract['index'] = range(points)
-                        extract[['index', 'date', 'cost']].to_csv(fileout + '.csv', index=False)
-                        i += 1
-                    else:
-                        print 'NOT WRITING ', domain, zone, 'because not enough samples:', points, 'Volume:', total
-                except Exception as e:
-                    # print 'exception', e, type(e)
-                    continue
+    for idx in output.index.map(lambda x: x[:-1]).unique():
+        extract = output.ix[idx]
+        points = extract.shape[0]
+        total = int(np.sum(extract))
+        title = root + 'T' + str(total) + '_' + 'P' + str(points) + get_title(idx)
+        plt.plot(extract, marker='.')
+        plt.xlabel(grouped_cols[-1])
+        plt.ylabel(agg_col[0])
+        plt.title(get_title(idx))
+        plt.savefig(title)
+        plt.clf()
+        extract['date'] = extract.index
+        extract.index = range(points)
+        extract['index'] = range(points)
+        cols_out = ['index', 'date'] + agg_col
+        extract[cols_out].to_csv(title + '.csv', index=False)
+    return
 
 
 def format_cls_expenses():
@@ -179,58 +131,9 @@ def format_cls_expenses():
     combined = add_cls_expenses_conformed_columns(combined)
     agg_col = ['cost']
     grouping_cols = ['cost_type', 'Domain', 'Zone', 'Fiscal Period']
+    combined['fiscal_year'] = combined['Fiscal Period'].apply(lambda x: get_fiscal_year(x))
+    grouping_cols = ['cost_type', 'Domain', 'Zone', 'fiscal_year']
     file_agg_writer(combined, grouping_cols, agg_col)
-    return
-    grouped = output.groupby(grouping_cols)
-
-    # print tv['Domain'].unique()
-    output = grouped.aggregate(np.sum)
-    cost_types = output.index.levels[0]
-    domains = output.index.levels[1]
-    zones = output.index.levels[2]
-    i = 1
-    for cost_type in cost_types:
-        for domain in domains:
-            for zone in zones:
-                try:
-                    extract = output.loc[cost_type, domain, zone]
-                    print extract.head()
-                    print type(extract.index[0])
-                    plt.plot(extract, marker='.')
-                    plt.xlabel('Year')
-                    plt.ylabel('Cost')
-                    title = cost_type + ' ' + domain + ' ' + zone
-                    plt.title(title)
-                    plt.show()
-                    plt.clf()
-                # points = extract.shape[0]
-                # total = np.sum(extract)
-                # # print points, total, extract
-                # if points > 24:
-                #     title = domain + ' ' + zone
-                #     fileout = 'processed\\cost\\C' + str(total) + '_N' + str(points) + '_' + domain + '_' + zone
-                #     fileout = fileout.replace(' ', '_')
-                #     plt.plot(extract, marker='.')
-                #     plt.xlabel('Year')
-                #     plt.ylabel('Cost')
-                #     plt.title(title)
-                #     plt.savefig(fileout)
-                #     plt.clf()
-                #     print 'writing ' + fileout
-                #     extract['date'] = extract.index
-                #     extract.index = range(points)
-                #     extract['index'] = range(points)
-                #     extract[['index', 'date', 'Count']].to_csv(fileout + '.csv', index=False)
-                #     i += 1
-                # else:
-                #     print 'NOT WRITING ', domain, zone, 'because not enough samples:', points, 'Cost:', total
-                except Exception as e:
-                    print 'exception', e
-                    continue
-
-    # print output
-    #
-    # output[['fn_cd', 'fn_desc', 'fin_group', 'Department', 'Domain']].to_csv('temp.csv')
 
 
 def format_ahs_expenses():
@@ -262,11 +165,23 @@ def format_ahs_expenses():
     input['cost'] = input['cost'].fillna(0)
     return input
 
+def get_fiscal_year(datetime):
+    year = datetime.year
+    month = datetime.month
+    if month < 4:
+        return year
+    else:
+        return year + 1
+
+
+
 
 if __name__ == "__main__":
-    # preprocess_volumes_data()
+    preprocess_volumes_data()
     # df = load_cls_expenses()
     # add_cls_expenses_conformed_columns(df)
-    format_cls_expenses()
+    # format_cls_expenses()
     # parse_bill_code('221.0000.71105009975')
     # print format_ahs_expenses()
+
+    # print get_fiscal_year(date(2008, 12, 24))
